008030d47fbe8f4220ce89837bda069737a1a48b
Ana <ana_l_amaral@estudante.sesisenai.org.br>
2025-08-22 10:05:28 -0300

    Add - names

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,7 @@
 </div> <!-- fecha container -->
 
 <footer class="bg-dark text-light text-center py-3 mt-4 rodape">
-  <p>Futebol CRUD © <?= date("Y") ?></p>
+  <p>Futebol CRUD © <br> Por Ana Amaral, João Medeiros, Kamila Santos e Amanda Moller <br> <?= date("Y") ?></p>
 </footer>
 
 <script src="/CRUD_Futebol/futebol_em_crud/assets/js/bootstrap.bundle.min.js"></script>
